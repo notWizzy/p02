e1e540d02aa9cc15331510117c889f9a029fc5d5
Dmool <143208768+dmool@users.noreply.github.com>
2023-09-18 13:00:38 -0400

    Created the TemplateProcessor class and the fillIn method

--- template-processor.js
+++ template-processor.js
@@ -4,14 +4,16 @@ class TemplateProcessor {
         this.template = template;
     }
 
-    //method
-    fillIn(dictionary) {
-        return this.template.replace(/\{\{(\w+)}}/g, (match, value) =>{
-            if (dictionary.hasOwnProperty(value)){
-                return dictionary[value];
-            } else{
-                return " ";
+    //Method to fill the template with the dictionary
+    fillIn(dictionary){
+        let returnString = this.template;
+        for (const property in dictionary){
+            if (Object.prototype.hasOwnProperty.call(dictionary, property)) {
+                returnString = returnString.replaceAll("{{" + property + "}}",dictionary[property]);
             }
-        });
+        }
+        const regex = /{{.*}}/g;
+        returnString = returnString.replaceAll(regex,"");
+        return returnString;
     }
 }
