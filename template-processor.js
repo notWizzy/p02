457fb05bd0cdfe580d562763a6494ebb35163045
Dmool <143208768+dmool@users.noreply.github.com>
2023-09-17 23:17:02 -0400

    Created the TemplateProcessor class and the fillIn method

--- template-processor.js
+++ template-processor.js
@@ -1,8 +1,10 @@
+//TemplateProcessor Class
 class TemplateProcessor {
     constructor(template) {
         this.template = template;
     }
 
+    //method
     fillIn(dictionary) {
         return this.template.replace(/\{\{(\w+)}}/g, (match, value) =>{
             if (dictionary.hasOwnProperty(value)){
